8054743b8ae9f17a1712f1068b4a7f315caf9f9e
Nicole Liu <nicoleliu1016@gmail.com>
2026-04-21 23:02:48 -0700

    update

--- backend/JTA/settings.py
+++ backend/JTA/settings.py
@@ -119,7 +119,7 @@ CORS_ALLOW_CREDENTIALS = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_NAME'),
+        'NAME': config('POSTGRES_DB'),
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST': config('POSTGRES_HOST', default='localhost'),
