ce3f6dca0641b67e385febf7e64f0c84d895c061
Nicole Liu <nicoleliu1016@gmail.com>
2026-04-21 18:40:40 -0700

    update settings

--- backend/JTA/settings.py
+++ backend/JTA/settings.py
@@ -18,7 +18,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-env_path = os.path.join(BASE_DIR, "/backend/.env")
+env_path = os.path.join(BASE_DIR, "backend/.env")
 
 config = Config(RepositoryEnv(env_path))
 
